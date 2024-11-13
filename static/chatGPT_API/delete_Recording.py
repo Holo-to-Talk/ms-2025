4e866c6441aa44d516dcc0c5f8b7d2f605529af7
kaito0320 <hal.kaito.0320.mikan.tokyo@gmail.com>
2024-11-13 18:00:36 +0900

    ファイル削除

--- static/chatGPT_API/delete_Recording.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def delete_Recording(savedDirectory):
-    # ファイルが存在するか確認
-    if os.path.exists(savedDirectory):
-        os.remove(savedDirectory)
-        print(f"File {savedDirectory} deleted")
-    else:
-        print(f"File {savedDirectory} does not exist")
