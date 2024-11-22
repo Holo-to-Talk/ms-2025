ce04a483e18d8820abb934ed16e9f02e940989ee
kaito0320 <hal.kaito.0320.mikan.tokyo@gmail.com>
2024-11-22 19:29:07 +0900

    id変更

--- static/ai-app/static/js/socket.js
+++ static/ai-app/static/js/socket.js
@@ -19,9 +19,9 @@ socket.on('update_output', (data) => {
 });
 
 socket.on('update_telop_add_display_none', () => {
-    document.getElementById('telop').classList.add('display_none');
+    document.getElementById('div_telop').classList.add('display_none');
 });
 
 socket.on('update_telop_remove_display_none', () => {
-    document.getElementById('telop').classList.remove('display_none');
+    document.getElementById('div_telop').classList.remove('display_none');
 });
