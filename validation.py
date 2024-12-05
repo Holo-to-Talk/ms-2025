eebbf4bba51b75e772081891e93029ebfb02749e
K1zunaaa <contact@k1zuna.net>
2024-12-06 00:31:49 +0900

    APIキー桁数微調整

--- validation.py
+++ validation.py
@@ -27,22 +27,22 @@ def validate_phone_num(phone_num):
 def validate_app_sid(app_sid):
     if not app_sid.encode('utf-8').isalnum():
         return "twilio_twiml_app_sidは半角英数字のみを入力してください。"
-    if len(app_sid) != 35:
-        return "twilio_twiml_app_sidは半角英数字35文字です。"
+    if len(app_sid) != 34:
+        return "twilio_twiml_app_sidは半角英数字34文字です。"
     return ""
 
 def validate_app_key(app_key):
     if not app_key.encode('utf-8').isalnum():
         return "api_keyは半角英数字のみを入力してください。"
-    if len(app_key) != 35:
-        return "api_keyは半角英数字35文字です。"
+    if len(app_key) != 34:
+        return "api_keyは半角英数字34文字です。"
     return ""
 
 def validate_app_secret(app_secret):
     if not app_secret.encode('utf-8').isalnum():
         return "api_secretは半角英数字のみを入力してください。"
-    if len(app_secret) != 33:
-        return "api_secretは半角英数字33文字です。"
+    if len(app_secret) != 32:
+        return "api_secretは半角英数字32文字です。"
     return ""
 
 def validate_password(password):
