8a572db72db14c918f751e5961da04c2b93737d7
kaito0320 <hal.kaito.0320.mikan.tokyo@gmail.com>
2024-12-10 13:28:26 +0900

    スペル変更

--- static/ai-app/app.py
+++ static/ai-app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, emit
 from socketio_Config import socketio
 from dotenv import load_dotenv
-from constatns import AppSettings
+from constants import AppSettings
 import os
 import time
 
